dfdbc45379da0da04419ba57afcd6169beb0fcea
Dhruv Jain <dhruv.jain93@gmail.com>
2020-10-21 21:38:01 +0530

    [Ref]: Rename ProfileScreen root component for better naming convention

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
-import profile from './components/profileScreen/profile.js';
 import {headerBackgroundColor, headerShadowColor} from './components/commons/cssVariables';
+import ProfileScreen from './components/profileScreen/index.js';
+
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -15,7 +16,7 @@ export default function App() {
           shadowColor: headerShadowColor,
         }
         }}> 
-        <Stack.Screen name="Profile" component={profile} options={{title: ""}}/>
+        <Stack.Screen name="Profile" component={ProfileScreen} options={{title: ""}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/profileScreen/index.js
+++ components/profileScreen/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import backgroundStyle from './../commons/backgroundStyle';
-export default function profile(){
+import backgroundStyle from '../commons/backgroundStyle';
+export default function ProfileScreen(){
     return (
         <View style={backgroundStyle.container}>
             
