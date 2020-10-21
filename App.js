ed76961cf4f8b2ff2b83ebe6fa1218688881c1a4
Dhruv Jain <dhruv.jain93@gmail.com>
2020-10-21 19:26:52 +0530

    [Ftr]: Add react-native navigation stack routing for ProfileScreen
    - Installed navigation related packages
    - Create a screen named Profile
    - Set the default style for the header

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import profile from './components/profileScreen/profile.js';
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>The Nagrik App</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerStyle:{backgroundColor: '#262626'},
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        }
+        }}> 
+        <Stack.Screen name="Profile" component={profile} options={{title: ""}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/profileScreen/profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, NativeModules} from 'react-native';
+
+export default function profile(){
+    return (
+        <View>
+            <Text>Profile</Text>
+        </View>
+    );
+}
